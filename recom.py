d077632c430ac4bbd33c68789a4651d1bd86ca5f
bsyj026 <bsyj026@koreanssam.kr>
2025-08-06 16:44:07 +0900

    Update recom.py

--- recom.py
+++ recom.py
@@ -25,7 +25,7 @@ client = OpenAI(
 )
 
 if menu == "홈":
-    st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/768px-ChatGPT-Logo.svg.png", width = 25)
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/768px-ChatGPT-Logo.svg.png", width = 25)
     st.header("홈 페이지")
     st.markdown('---')
     st.markdown("ai의 한마디")
@@ -105,6 +105,7 @@ if menu == '할 짓 추천':
                     response += chunk.choices[0].delta.content
                     msg_placeholder.markdown(response)
             st.session_state["messages"].append({"role":"assistant", "content":response})
+
 
 
 
