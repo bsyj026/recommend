05ac84a26f924ccbc7b7fdd717b69dc8690bfe16
bsyj026 <bsyj026@koreanssam.kr>
2025-08-06 16:08:25 +0900

    Update recom.py

--- recom.py
+++ recom.py
@@ -4,8 +4,9 @@ import requests
 import json
 
 
-st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/768px-ChatGPT-Logo.svg.png", caption="사실은 지피티 아님")
+
 st.sidebar.title("메뉴")
+st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/768px-ChatGPT-Logo.svg.png", caption="사실은 지피티 아님", use_column_width=True)
 menu = st.sidebar.selectbox("", ["홈", "설정", "할 짓 추천"])
 user_setting = ""
 
@@ -105,6 +106,7 @@ if menu == '할 짓 추천':
                     response += chunk.choices[0].delta.content
                     msg_placeholder.markdown(response)
             st.session_state["messages"].append({"role":"assistant", "content":response})
+
 
 
 
