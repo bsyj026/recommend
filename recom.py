18d716f2f51ea97365267ea4b5d11ec05d027f2e
bsyj026 <bsyj026@koreanssam.kr>
2025-08-07 13:56:39 +0900

    Update recom.py

--- recom.py
+++ recom.py
@@ -79,9 +79,11 @@ elif menu == "설정":
         if get != '':
             user_state = f'사용자는 지금 {where}에 있고 {get}을 가지고 있음'
             st.session_state["user_setting"] = user_setting
+            st.session_state["user_location"] = where
         if mode == True:
             user_setting += '싸가지 없게 말해. 무조건 싸가지 없게. 인성을 국밥 말아먹은 듯이. 세상에서 제일 개쓰레기처럼. 비꼬는 듯이. 세상 만사에 불만이 있는 씨발년처럼. 비속어를 섞어서'
             st.session_state["user_setting"] = user_setting
+            st.session_state["user_location"] = where
 
 if menu == '할 짓 추천':
     setting_prompt = st.session_state.get("user_setting", "설정 정보 없음")
@@ -118,6 +120,7 @@ if menu == '할 짓 추천':
                     response += chunk.choices[0].delta.content
                     msg_placeholder.markdown(response)
             st.session_state["messages"].append({"role":"assistant", "content":response})
+
 
 
 
