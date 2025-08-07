8c223c50a2ab09f66e1dbff4b00bc162d7e5b6fa
bsyj026 <bsyj026@koreanssam.kr>
2025-08-07 13:30:23 +0900

    Update recom.py

--- recom.py
+++ recom.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import requests
 import json
 
+st.session_state["user_location"] = '야외'
 st.sidebar.title("메뉴")
 menu = st.sidebar.selectbox("", ["홈", "설정", "할 짓 추천"])
 user_setting = ""
@@ -47,7 +48,10 @@ if menu == "홈":
 
 elif menu == "설정":
     st.header("설정:")
-    where = st.selectbox("당신의 위치:",["야외","실내"])
+    if st.session_state.get("user_location", "설정 정보 없음") == '야외':
+        where = st.selectbox("당신의 위치", ["야외", "실내"])
+    else:
+        where = st.selectbox("당신의 위치", ["야외", "실내"], index = 1)
     get = st.text_input("당신이 가지고있는 것:")
     
     mode = st.checkbox("심심이 모드(심약자 및 변태를 제외한 모든 욕을 먹기 싫어하는 일반인에게 추천되지 않는다.)")
@@ -62,8 +66,10 @@ elif menu == "설정":
     if passs == True:
         if get == "":
             user_setting = f"사용자의 위치는 {where}"
+            st.session_state["user_location"] = where
         else:
             user_setting = f"사용자의 위치는 {where}이고, 사용자가 가지고 있는건 {get}이다."
+            st.session_state["user_location"] = where
     
 
         st.text("설정이 완료되었습니다!")
@@ -109,3 +115,4 @@ if menu == '할 짓 추천':
                     response += chunk.choices[0].delta.content
                     msg_placeholder.markdown(response)
             st.session_state["messages"].append({"role":"assistant", "content":response})
+
