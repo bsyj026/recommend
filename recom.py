16a36d5031863df5c50ed010e2cdfe69e6f2edfb
bsyj026 <bsyj026@koreanssam.kr>
2025-08-07 13:30:58 +0900

    Update recom.py

--- recom.py
+++ recom.py
@@ -51,7 +51,7 @@ elif menu == "설정":
     if st.session_state.get("user_location", "설정 정보 없음") == '야외':
         where = st.selectbox("당신의 위치", ["야외", "실내"])
     else:
-        where = st.selectbox("당신의 위치", ["야외", "실내"], index = 1)
+        where = st.selectbox("당신의 위치", ["야외", "실내"], index = 2)
     get = st.text_input("당신이 가지고있는 것:")
     
     mode = st.checkbox("심심이 모드(심약자 및 변태를 제외한 모든 욕을 먹기 싫어하는 일반인에게 추천되지 않는다.)")
@@ -115,4 +115,5 @@ if menu == '할 짓 추천':
                     response += chunk.choices[0].delta.content
                     msg_placeholder.markdown(response)
             st.session_state["messages"].append({"role":"assistant", "content":response})
+
 
